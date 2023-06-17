04b7a08fb7baa3ab39890a028c3332cc324c4c34
priyadarshii786 <tathagatp918@gmail.com>
2023-06-18 01:24:05 +0530

    ....

---
+++ src/Intern_Learn/Learn_4_main_Question_1.java
@@ -0,0 +1,29 @@
+package Intern_Learn;
+public class Learn_4_main_Question_1 {
+    int a = 10;
+//    static int a = 10;
+    int b = 15;
+//    static int b = 10;
+
+
+    public static void main(String[] args) {
+        Learn_4_main_Question_1 kim_jong = new Learn_4_main_Question_1();
+        System.out.println(kim_jong.a);
+        System.out.println(kim_jong.a + kim_jong.b);
+        System.out.println(kim_jong.a - kim_jong.b);
+        System.out.println(kim_jong.a * kim_jong.b);
+    }
+
+
+
+
+//    public static void main(String[] args) {
+//        System.out.println(a+b);
+//        System.out.println(a-b);
+//        System.out.println(a*b);
+//        aisa tb hi likh skte hai jb a and b dono static ho which means ki uska object banane ka jarurat nahi hai within the class.
+//    }
+
+}
+
+
