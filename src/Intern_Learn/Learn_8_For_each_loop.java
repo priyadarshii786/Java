dd60c1a6b5bedfe907f6a430e9e1548f5baff318
priyadarshii786 <tathagatp918@gmail.com>
2023-06-18 01:48:54 +0530

    ....

---
+++ src/Intern_Learn/Learn_8_For_each_loop.java
@@ -0,0 +1,12 @@
+package Intern_Learn;
+public class Learn_8_For_each_loop {
+//    for-each loop is basically used for the iteration purpose.
+    public static void main(String[] args) {
+        int chikku[] = {5,10,15,20};
+
+        for (int i:chikku)
+        {
+            System.out.println(i);
+        }
+    }
+}
